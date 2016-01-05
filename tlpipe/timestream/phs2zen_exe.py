539939d6ddc4c18f6b18c8b157c0bd42c4a22b56
zuoshifan <zuoshifan@163.com>
2016-01-05 19:18:38 +0800

    Work for hdf5 in phs2zen

--- tlpipe/timestream/phs2zen_exe.py
+++ tlpipe/timestream/phs2zen_exe.py
@@ -23,12 +23,7 @@ params_init = {
                'nprocs': mpiutil.size, # number of processes to run this module
                'aprocs': range(mpiutil.size), # list of active process rank no.
                # 'data_dir': './',  # directory the data in
-               'data_file': './data.npy',
-               'freq_file': './data_freq.npy',
-               'time_file': './data_time.npy',
-               'ants_file': './data_ants.npy',
-               'az': 0.0, # degree
-               'alt': 90.0, # degree
+               'data_files': ['./data.hdf5'],
                'output_dir': './output/', # output directory
               }
 prefix = 'ph_'
@@ -63,16 +58,20 @@ class Phs2zen(object):
             if not os.path.exists(output_dir):
                 os.mkdir(output_dir)
 
-        data = np.load(self.params['data_file']) # pol: xx, yy, xy, yx
-        ants = np.load(self.params['ants_file'])
-        freq = np.load(self.params['freq_file']) # MHz
-        ts = np.load(self.params['time_file']) # Julian date
+        data_files = self.params['data_files']
+        nfiles = len(data_files)
+        assert nfiles > 0, 'No input data files'
 
-        data_phs2zen = np.zeros_like(data) # save data that phased to zenith
-        data_int_time = np.zeros(data.shape[1:], dtype=data.dtype) # save data integrage over time
+        # read in ants, freq, time info from data files
+        with h5py.File(data_files[0], 'r') as f:
+            dataset = f['data']
+            data_shp = dataset.shape
+            data_type = dataset.dtype
+            ants = dataset.attrs['ants']
+            freq = dataset.attrs['freq']
+            # ts = f['time'] # Julian date for data in this file only
 
-        npol = data.shape[2]
-        nt = len(ts)
+        npol = data_shp[2]
         nfreq = len(freq)
         # cut central 80% of the data
         # data[np.int(0.1*nt):np.int(0.9*nt)] = 0
@@ -86,18 +85,41 @@ class Phs2zen(object):
         nbls = len(bls)
 
 
-        if self.comm is not None:
-            assert self.comm.size <= nbls, 'Can not have nprocs (%d) > nbls (%d)' % (self.comm.size, nbls)
+        # if self.comm is not None:
+        #     assert self.comm.size <= nbls, 'Can not have nprocs (%d) > nbls (%d)' % (self.comm.size, nbls)
 
+
+        lbls, sbl, ebl = mpiutil.split_local(nbls)
+        local_bls = range(sbl, ebl)
+        # local data section corresponding to local bls
+        local_data = np.array([], dtype=data_type).reshape((0, lbls, npol, nfreq))
+        ts = np.array([], dtype=np.float64)
+        for ind, data_file in enumerate(data_files):
+            with h5py.File(data_file, 'r') as f:
+                local_data = np.concatenate((local_data, f['data'][:, sbl:ebl, :, :]), axis=0)
+                ts = np.concatenate((ts, f['time'][...])) # Julian date
+        local_int_time = np.zeros(local_data.shape[1:], dtype=local_data.dtype)
+
+        nt = len(ts)
+        if mpiutil.size == 1:
+            data_phs2zen = local_data.view()
+            data_int_time = local_int_time.view()
+        else:
+            if mpiutil.rank0:
+                data_phs2zen = np.zeros((nt,) + data_shp[1:], dtype=data_type) # save data that phased to zenith
+                data_int_time = np.zeros(data_phs2zen.shape[1:], dtype=data_type) # save data integrate over time
+            else:
+                data_phs2zen = None
+                data_int_time = None
 
         for pol_ind in range(npol):
-            for bl_ind in mpiutil.mpirange(nbls): # mpi among bls
+            for bi, bl_ind in enumerate(local_bls): # mpi among bls
                 # # ignore auto-correlation
                 # bl = bls[bl_ind]
                 # if bl[0] == bl[1]:
                 #     continue
 
-                data_slice = data[:, bl_ind, pol_ind, :]
+                data_slice = local_data[:, bi, pol_ind, :].copy() # will use local_data to save data_slice_dphs in-place, so here use copy
 
                 # subtract the mean
                 # data -= np.mean(data, axis=1)
@@ -116,7 +138,7 @@ class Phs2zen(object):
                 # find max in time fft
                 max_row_ind = np.argmax(np.abs(data_slice_fft_time), axis=0)
                 data_slice_fft_time_max = np.zeros_like(data_slice_fft_time)
-                for ci in range(len(freq)):
+                for ci in range(nfreq):
                     data_slice_fft_time_max[max_row_ind[ci], ci] = data_slice_fft_time[max_row_ind[ci], ci]
 
                 # ifft for time
@@ -138,7 +160,8 @@ class Phs2zen(object):
                 # fill invalid values (divide 0 or something else) by 0.0
                 data_slice_dphs[np.logical_not(np.isfinite(data_slice_dphs))] = 0.0
                 # save data after phas2 to zenith
-                data_phs2zen[:, bl_ind, pol_ind, :] = data_slice_dphs
+                # data_phs2zen[:, bl_ind, pol_ind, :] = data_slice_dphs
+                local_data[:, bi, pol_ind, :] = data_slice_dphs # change local_data to save memory
 
                 # freq fft
                 data_slice_dphs_fft_freq = np.fft.fft(data_slice_dphs, axis=1)
@@ -182,7 +205,7 @@ class Phs2zen(object):
 
                 # integrate over time
                 data_slice_int_time = np.sum(data_slice_dphs, axis=0)
-                data_int_time[bl_ind, pol_ind, :] = data_slice_int_time
+                local_int_time[bi, pol_ind, :] = data_slice_int_time
 
                 # save data to file
                 filename = output_dir + 'data_slice_%d_%d_%s.hdf5' % (bls[bl_ind][0], bls[bl_ind][1], pol_dict[pol_ind])
@@ -205,27 +228,37 @@ class Phs2zen(object):
                     f.create_dataset('data_slice_int_time', data=data_slice_int_time)
 
 
-        # Reduce data in separate processes
+        # Gather data in separate processes
         if self.comm is not None and self.comm.size > 1: # Reduce only when there are multiple processes
-            if mpiutil.rank0:
-                self.comm.Reduce(mpiutil.IN_PLACE, data_phs2zen, op=mpiutil.SUM, root=0)
-            else:
-                self.comm.Reduce(data_phs2zen, data_phs2zen, op=mpiutil.SUM, root=0)
-            if mpiutil.rank0:
-                self.comm.Reduce(mpiutil.IN_PLACE, data_int_time, op=mpiutil.SUM, root=0)
-            else:
-                self.comm.Reduce(data_int_time, data_int_time, op=mpiutil.SUM, root=0)
+            mpiutil.gather_local(data_phs2zen, local_data, (0, sbl, 0, 0), root=0, comm=self.comm)
+            mpiutil.gather_local(data_int_time, local_int_time, (sbl, 0, 0), root=0, comm=self.comm)
+
+
+            # if mpiutil.rank0:
+            #     self.comm.Reduce(mpiutil.IN_PLACE, data_phs2zen, op=mpiutil.SUM, root=0)
+            # else:
+            #     self.comm.Reduce(data_phs2zen, data_phs2zen, op=mpiutil.SUM, root=0)
+            # if mpiutil.rank0:
+            #     self.comm.Reduce(mpiutil.IN_PLACE, data_int_time, op=mpiutil.SUM, root=0)
+            # else:
+            #     self.comm.Reduce(data_int_time, data_int_time, op=mpiutil.SUM, root=0)
 
         # save data phased to zenith
         if mpiutil.rank0:
             with h5py.File(output_dir + 'data_phs2zen.hdf5', 'w') as f:
-                dset = f.create_dataset('data_phs2zen', data=data_phs2zen)
-                dset.attrs['ants'] = ants
-                dset.attrs['ts'] = ts
-                dset.attrs['freq'] = freq
-                dset.attrs['bls'] = pickle.dumps(bls) # save as list
-                dset.attrs['az'] = self.params['az']
-                dset.attrs['alt'] = self.params['alt']
-            # save data integrate over time
-            with h5py.File(output_dir + 'data_int_time.hdf5', 'w') as f:
+                f.create_dataset('time', data=ts)
                 f.create_dataset('data_int_time', data=data_int_time)
+                dset = f.create_dataset('data', data=data_phs2zen)
+                # copy metadata from input file
+                with h5py.File(data_files[0], 'r') as fin:
+                    for attrs_name, attrs_value in fin['data'].attrs.iteritems():
+                        dset.attrs[attrs_name] = attrs_value
+                # dset.attrs['ants'] = ants
+                # dset.attrs['ts'] = ts
+                # dset.attrs['freq'] = freq
+                # dset.attrs['bls'] = pickle.dumps(bls) # save as list
+                # dset.attrs['az'] = self.params['az']
+                # dset.attrs['alt'] = self.params['alt']
+            # # save data integrate over time
+            # with h5py.File(output_dir + 'data_int_time.hdf5', 'w') as f:
+            #     f.create_dataset('data_int_time', data=data_int_time)
