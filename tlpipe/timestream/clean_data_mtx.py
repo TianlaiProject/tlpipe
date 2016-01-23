0ec2c0e9f513ec5600c5a2932db99abe44d9d21f
POFK <maotianxiang@bao.ac.cn>
2016-01-23 16:55:57 +0800

    2016/01/23_16:55:57

--- tlpipe/timestream/clean_data_mtx.py
+++ tlpipe/timestream/clean_data_mtx.py
@@ -19,7 +19,7 @@ params_init = {
                'input_dir': '/home/mtx/data/Tianlai/', 
                'output_file': 'output.hdf5',
                'output_dir': '/home/mtx/data/Tianlai/',
-               'Plot_test': False  # To test the result.
+               'Plot_test': False # To test the result.
               }
 prefix = 'CD_'
 #*******************************************************************************
@@ -40,25 +40,32 @@ class CleanData(Base):
             sigma = params_init['sigma']
             time= params_init['time']
             Plot_test= params_init['Plot_test']
+
+#           input=self.params['input_file']
+#           input_dir=self.params['input_dir']
+#           output=self.params['output_file']
+#           output_dir=params_init['output_dir']
+#           sigma = params_init['sigma']
+#           time= params_init['time']
+#           Plot_test= params_init['Plot_test']
+
             f=h5py.File(input,'r')
-            g=h5py.File(output,'w')
             shape=f['data'].shape
             dtype=f['data'].dtype
             data_input=f['data'][...]
             f.close()
             print shape,dtype
-            dset = g.create_dataset(name='data',shape=shape,dtype=dtype)
+#           dset = g.create_dataset(name='data',shape=shape,dtype=dtype)
     
             for bls in np.arange(shape[1]):
                 for pol in np.arange(shape[2]):
     #               data=f['data'][:,bls,pol,:]
-                    data=data_input[:,bls,pol,:]
-                    s = data[time]
+                    s = data_input[:,bls,pol,:][time]
                     index_all = []
     
                     if Plot_test:
                         if bls<3:
-                            plt.imshow(data[8500:9000].real)
+                            plt.imshow(data_input[:,bls,pol,:][8500:9000].real)
                             plt.title('test_clean_%d_%d_origin.eps'%(bls,pol))
                             plt.colorbar()
                             plt.xlim(0, 512)
@@ -95,14 +102,16 @@ class CleanData(Base):
                     index = index[np.argsort(index)]
                     print 'cleaning baseline:%d pol:%d ...'%(bls,pol),index
                     
-                    data[:,index] = np.nan
-                    dset[:,bls,pol,:]=data
+                    data_input[:,bls,pol,index] = np.nan
+
                     if Plot_test:
                         if bls<3:
-                            plt.imshow(data[8500:9000].real)
+                            plt.imshow(data_input[:,bls,pol,:][8500:9000].real)
                             plt.title('test_clean_%d_%d'%(bls,pol))
                             plt.colorbar()
                             plt.xlim(0, 512)
                             plt.savefig(output_dir+'test_clean_%d_%d.eps'%(bls,pol))
                             plt.clf()
+            g=h5py.File(output,'w')
+            g.create_dataset(name='data',data=data_input)
             g.close()
