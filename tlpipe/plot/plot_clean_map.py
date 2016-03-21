3b57b576e1437fce3ce24712bb88fe6831a06740
ycli <ycli@bao.ac.cn>
2016-03-21 10:19:51 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch ycli_dev  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	new file:   input/ycli/cas5hrfi.pipe
    	new file:   input/ycli/sim_longbl_Cyg.pipe
    	new file:   input/ycli/sim_longbl_dim.pipe
    	modified:   tlpipe/plot/plot_clean_map.py
    
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	input/ycli/out.e
    	input/ycli/out.o

--- tlpipe/plot/plot_clean_map.py
+++ tlpipe/plot/plot_clean_map.py
@@ -19,11 +19,14 @@ params_init = {
                'aprocs': range(mpiutil.size), # list of active process rank no.
                'input_file': 'clean_image.hdf5', # str or a list of str
                'output_file': None, # None, str or a list of str
+               'uv_input_file' : 'uv_image.hdf5',
                'catalog' : None,
                'fov': 10, # degree
                'cmap': 'jet',
                'vmin': None,
                'vmax': None,
+               #'ra_range': [0, 360],
+               #'dec_range': [-90, 90],
               }
 prefix = 'pcm_'
 
@@ -45,8 +48,11 @@ class Plot(Base):
             catalog = None
 
         input_file = input_path(self.params['input_file'])
+        uv_input_file = input_path(self.params['uv_input_file'])
         output_file = self.params['output_file']
         fov = self.params['fov']
+        #ra_min,  ra_max  = self.params['ra_range']
+        #dec_min, dec_max = self.params['dec_range']
         cmap = self.params['cmap']
         vmin = self.params['vmin']
         vmax = self.params['vmax']
@@ -56,23 +62,38 @@ class Plot(Base):
         else:
             output_file = output_path(output_file)
 
-        with h5py.File(input_file, 'r') as f:
+        with h5py.File(input_file, 'r') as f: 
             img = f['cimc'][...]
             ra = f.attrs['ra']
             dec = f.attrs['dec']
             d_ra = f.attrs['d_ra']
             d_dec = f.attrs['d_dec']
 
-        shape = img.shape
-        RA  = ( np.arange(shape[0] + 1) - shape[0]//2 ) * d_ra  + ra
-        DEC = ( np.arange(shape[1] + 1) - shape[1]//2 ) * d_dec + dec 
+        with h5py.File(uv_input_file, 'r') as uvf:
+            max_wl = uvf.attrs['max_lm']
+            uv_fft = uvf['uv_fft'][...]
 
-        RA  -= 0.5 * d_ra
-        DEC -= 0.5 * d_dec
+
+        shape = img.shape
+
+        space = np.linspace(-max_wl, max_wl, shape[0]+1)
+        space = np.arcsin(space) * 180./np.pi
+        RA  = space + ra
+        DEC = space + dec
+
+        #RA  = ( np.arange(shape[0] + 1) - shape[0]//2 ) * d_ra  + ra
+        #DEC = ( np.arange(shape[1] + 1) - shape[1]//2 ) * d_dec + dec 
+
+        #RA  -= 0.5 * d_ra
+        #DEC -= 0.5 * d_dec
 
         fig = plt.figure(figsize=(5, 5))
-        ax  = fig.add_axes([0.15, 0.15, 0.8, 0.8])
-        ax.pcolormesh(RA[None, :], DEC[:, None], img.real)
+        ax  = fig.add_axes([0.15, 0.15, 0.65, 0.8])
+        cax = fig.add_axes([0.83, 0.25, 0.03, 0.6])
+        #ax.pcolormesh(RA[None, :], DEC[:, None], img.real)
+        im = ax.pcolormesh(RA[None, :], DEC[:, None], uv_fft.real, cmap='Greys')
+        fig.colorbar(im, ax=ax, cax=cax)
+
         ax.set_xlim(xmin=ra-0.5*fov, xmax=ra+0.5*fov)
         ax.set_ylim(ymin=dec-0.5*fov, ymax=dec+0.5*fov)
 
@@ -83,27 +104,53 @@ class Plot(Base):
         ax.tick_params(which='minor', length=2, width=1., direction='out')
         ax.set_aspect('equal')
 
+        cax.minorticks_on()
+        cax.tick_params(length=4, width=1., direction='out')
+        cax.tick_params(which='minor', length=2, width=1., direction='out')
+
         if catalog != None:
-            ax.scatter(catalog[:,0]*180./np.pi, catalog[:,1]*180./np.pi, 
-                    s=60, c='none', edgecolor='w')
+            #ax.scatter(catalog[:,0]*180./np.pi, catalog[:,1]*180./np.pi, 
+            #        s=30, c='none', edgecolor='g', mew=0.5)
+            ax.plot(catalog[:,0]*180./np.pi, catalog[:,1]*180./np.pi, 'go', 
+                    mec='g', mew=0.5, mfc='none', markersize=5)
 
-        plt.savefig(output_file)
+        plt.savefig(output_file.replace('.png', '_dirty.png'))
 
-        #xshp, yshp = img.shape
-        #nx = min(xshp, fov / np.abs(d_ra))
-        #ny = min(yshp, fov / np.abs(d_dec))
-        #img = img[(xshp - nx)/2:(xshp + nx)/2, (yshp - ny)/2:(yshp + ny)/2]
+        
+        vmax = None
+        vmin = None
+        #mean = np.mean(img.real)
+        #std  = np.std(img.real)
+        #print std, mean
+        #vmax = mean + 30 * std
+        #vmin = mean - 30 * std
 
-        #xpx, ypx = img.shape
-        #dx1 = -(xpx/2 + .5) * np.radians(d_ra)
-        #dx2 = (xpx/2 - .5) * np.radians(d_ra)
-        #dy1 = -(ypx/2 + .5) * np.radians(d_dec)
-        #dy2 = (ypx/2 - .5) * np.radians(d_dec)
+        fig = plt.figure(figsize=(5, 5))
+        ax  = fig.add_axes([0.15, 0.15, 0.65, 0.8])
+        cax = fig.add_axes([0.83, 0.25, 0.03, 0.6])
+        im = ax.pcolormesh(RA[None, :], DEC[:, None], img.real, 
+                cmap='Greys', vmax=vmax, vmin=vmin)
+        #im = ax.pcolormesh(RA[None, :], DEC[:, None], uv_fft.real, cmap='Greys')
+        fig.colorbar(im, ax=ax, cax=cax)
 
-        #plt.figure(figsize=(8, 6))
-        #map = Basemap(projection='ortho', lon_0=ra, lat_0=dec, rsphere=1, llcrnrx=dx1, llcrnry=dy1, urcrnrx=dx2, urcrnry=dy2)
-        #map.drawmeridians(np.arange(0, 360, 2), latmax=90.0, labels=[0, 0, 0, 1])
-        #map.drawparallels(np.arange(-90, 90, 2), labels=[1, 0, 0, 0])
-        #map.drawmapboundary()
-        #map.imshow(img.real, vmin=vmin, vmax=vmax, cmap=cmap, interpolation='nearest')
-        #plt.colorbar()
+        ax.set_xlim(xmin=ra-0.5*fov, xmax=ra+0.5*fov)
+        ax.set_ylim(ymin=dec-0.5*fov, ymax=dec+0.5*fov)
+
+        ax.set_ylabel('Dec')
+        ax.set_xlabel('RA')
+        ax.minorticks_on()
+        ax.tick_params(length=4, width=1., direction='out')
+        ax.tick_params(which='minor', length=2, width=1., direction='out')
+        ax.set_aspect('equal')
+
+        cax.minorticks_on()
+        cax.tick_params(length=4, width=1., direction='out')
+        cax.tick_params(which='minor', length=2, width=1., direction='out')
+
+        if catalog != None:
+            #ax.scatter(catalog[:,0]*180./np.pi, catalog[:,1]*180./np.pi, 
+            #        s=30, c='none', edgecolor='g', mew=0.5)
+            ax.plot(catalog[:,0]*180./np.pi, catalog[:,1]*180./np.pi, 'go', 
+                    mec='g', mew=0.5, mfc='none', markersize=5)
+
+        plt.savefig(output_file.replace('.png', '_clean.png'))
