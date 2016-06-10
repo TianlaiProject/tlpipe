54783b4c8ab2534856076634b1ad3d99699bb078
zuoshifan <zuoshifan@163.com>
2016-06-10 17:28:40 +0800

    Change in date_util

--- tlpipe/utils/date_util.py
+++ tlpipe/utils/date_util.py
@@ -1,18 +1,19 @@
 """Date and time utils."""
 
+import re
 import ephem
 
 
-def get_ephdate(local_time, tzone='UTC+08'):
+def get_ephdate(local_time, tzone='UTC+08h'):
     """Convert `local_time` to ephem utc date.
 
     Parameters
     ----------
-    local_time : string
-        Local time in any string format that can be recognized by ephem.
+    local_time : string, python date or datetime object, etc.
+        Local time that can be passed to ephem.Date function.
         Refer to http://rhodesmill.org/pyephem/date.html
     tzone : string, optional
-        Time zone in format 'UTC[+/-]xx'. Defaut: UTC+08.
+        Time zone in format 'UTC[+/-]xxh'. Defaut: UTC+08h.
 
     Returns
     -------
@@ -24,22 +25,24 @@ def get_ephdate(local_time, tzone='UTC+08'):
     `get_juldate`
     """
     local_time = ephem.Date(local_time)
-    tz = int(tzone[3:])
+    pattern = '[-+]?\d+'
+    tz = re.search(pattern, tzone).group()
+    tz = int(tz)
     utc_time = local_time - tz * ephem.hour
 
     return utc_time
 
 
-def get_juldate(local_time, tzone='UTC+08'):
+def get_juldate(local_time, tzone='UTC+08h'):
     """Convert `local_time` to Julian date.
 
     Parameters
     ----------
-    local_time : string
-        Local time in any string format that can be recognized by ephem.
+    local_time : string, python date or datetime object, etc.
+        Local time that can be passed to ephem.Date function.
         Refer to http://rhodesmill.org/pyephem/date.html
     tzone : string, optional
-        Time zone in format 'UTC[+/-]xx'. Defaut: UTC+08.
+        Time zone in format 'UTC[+/-]xxh'. Defaut: UTC+08h.
 
     Returns
     -------
