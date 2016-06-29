86ed9528acf34687cd0dbd6bc2496d2a0167387a
zuoshifan <zuoshifan@163.com>
2016-06-29 15:46:13 +0800

    New task rt2ts

---
+++ tlpipe/timestream/rt2ts.py
@@ -0,0 +1,19 @@
+"""Convert RawTimestream to Timestream."""
+
+import tod_task
+
+
+class Rt2ts(tod_task.SingleRawTimestream):
+    """Convert RawTimestream to Timestream."""
+
+    params_init = {
+                    'keep_dist_axis': False,
+                  }
+
+    prefix = 'r2t_'
+
+    def process(self, rt):
+        ts = rt.separate_pol_and_bl(self.params['keep_dist_axis'])
+        ts.add_history(self.history)
+
+        return ts
