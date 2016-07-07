Correct to radians

tlpipe/timestream/phs2src.py
@@ -53,7 +53,7 @@ class Phs2src(tod_task.SingleTimestream):
             raise RuntimeError('Unknown antenna type %s' % ts.attrs['telescope'])
 
         feedno = ts['feedno'][:].tolist()
-        antpointing = ts['antpointing'][-1, :, :]
+        antpointing = np.radians(ts['antpointing'][-1, :, :]) # radians
 
         ts.redistribute(0) # make time the dist axis
 
@@ -71,7 +71,7 @@ class Phs2src(tod_task.SingleTimestream):
         try:
             # convert an observing time to the ra_dec of the array pointing of that time
             src_time = get_ephdate(source, tzone=ts.attrs['timezone']) # utc time
-            aa.date = str(ephem.Date(src_time)) # utc time
+            # aa.date = str(ephem.Date(src_time)) # utc time
             # print 'date:', aa.date
             azs = antpointing[:, 0]
             alts = antpointing[:, 1]