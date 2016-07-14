34c8c3f8acbbbe202ab1329af47b735a78f6fa9c
zuoshifan <zuoshifan@163.com>
2016-07-14 20:16:17 +0800

    New plot_gain

---
+++ tlpipe/plot/plot_gain.py
@@ -0,0 +1,58 @@
+"""Plot gain."""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from caput import memh5
+from caput import mpiutil
+from tlpipe.pipeline.pipeline import SingleBase
+from tlpipe.utils.path_util import input_path, output_path
+
+
+class PlotGain(SingleBase):
+    """Plot gain."""
+
+    params_init = {
+                    'fig_name': 'gain',
+                  }
+
+    prefix = 'pg_'
+
+    def process(self, mg):
+        fig_prefix = self.params['fig_name']
+
+        # distributed along feed
+        mg.dataset_common_to_distributed('eigval', distributed_axis=1)
+        mg.dataset_common_to_distributed('gain', distributed_axis=1)
+        feed = mpiutil.scatter_array(mg.attrs['feed'])
+
+        for idx, fd in enumerate(feed):
+            plt.figure()
+            plt.subplot(411)
+            plt.imshow(mg['gain'].local_data[:, idx, 0, :].T.real, origin='lower') # xx
+            plt.colorbar()
+            plt.subplot(412)
+            plt.imshow(mg['gain'].local_data[:, idx, 0, :].T.imag, origin='lower') # xx
+            plt.colorbar()
+            plt.subplot(413)
+            plt.imshow(mg['gain'].local_data[:, idx, 1, :].T.real, origin='lower') # yy
+            plt.colorbar()
+            plt.subplot(414)
+            plt.imshow(mg['gain'].local_data[:, idx, 1, :].T.imag, origin='lower') # yy
+            plt.colorbar()
+
+            fig_name = '%s_%d.png' % (fig_prefix, fd)
+            fig_name = output_path(fig_name)
+            fig_dir = os.path.dirname(fig_name)
+            try:
+                os.makedirs(fig_dir)
+            except OSError:
+                pass
+            plt.savefig(fig_name)
+
+        return mg
+
+    def read_input(self):
+        input_file = input_path(self.params['input_files'])
+
+        return memh5.MemGroup.from_hdf5(input_file, distributed=True, hints=False)
