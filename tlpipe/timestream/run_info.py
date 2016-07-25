5a1a47f867ea8e4917e6f43903eb48fe3cf232ad
zuoshifan <zuoshifan@163.com>
2016-07-25 20:10:45 +0800

    Changes in run_info

--- tlpipe/timestream/run_info.py
+++ tlpipe/timestream/run_info.py
@@ -19,38 +19,34 @@ class RunInfo(tod_task.SingleRawTimestream):
 
         # rt.redistribute('baseline')
 
-        # the observer telescope
-        tl = ephem.Observer()
-        # tl.date = # the date for which the position should be computed (current time)
-        # tl.epoch = ephem.J2000 # epoch for which coordinates should be generated (year 2000)
-        tl.long = str(rt.attrs['sitelon']) # degree, longitude should be positive for east and negative west
-        tl.lat = str(rt.attrs['sitelat']) # degree, latitude should be positive north and negative south
-        tl.elev = rt.attrs['siteelev'] # m, elevation above sea level in meters
-        # tl.temp = 15.0 # temperature in degrees centigrade
-        # tl.pressure = 1010.0 # mB, atmospheric pressure in milibars (1010 mB)
-        # tl.horizon = 0.0 # degree, at what angle you consider an object to be rising or setting
+        az_alt = np.zeros((rt['sec1970'].local_data.shape[0], 2), dtype=np.float32) # radians
 
         if rt.is_dish:
             # antpointing = rt['antpointing'][-1, :, :] # degree
             # pointingtime = rt['pointingtime'][-1, :, :] # degree
-            az_alt = np.zeros((rt['sec1970'].local_data.shape[0], 2), dtype=np.float32) # radians
             az_alt[:, 0] = 0.0 # az
             az_alt[:, 1] = np.pi/2 # alt
         elif rt.is_cylinder:
-            az_alt = np.zeros((rt['sec1970'].local_data.shape[0], 2), dtype=np.float32) # radians
-            az_alt[:, 0] = 0.0 # az
+            az_alt[:, 0] = np.pi/2 # az
             az_alt[:, 1] = np.pi/2 # alt
         else:
             raise RuntimeError('Unknown antenna type %s' % ts.attrs['telescope'])
 
+        # array
+        aa = rt.array
+
+        # aa.date = # the date for which the position should be computed (current time)
+        # aa.epoch = ephem.J2000 # epoch for which coordinates should be generated (year 2000)
+        # aa.temp = 15.0 # temperature in degrees centigrade
+        # aa.pressure = 1010.0 # mB, atmospheric pressure in milibars (1010 mB)
+        # aa.horizon = 0.0 # degree, at what angle you consider an object to be rising or setting
+
         ra_dec = np.zeros_like(az_alt) # radians
-        time_zone = rt.attrs['timezone']
         for ti in range(az_alt.shape[0]):
-            sec = rt['sec1970'].local_data[ti]
             az, alt = az_alt[ti]
             az, alt = ephem.degrees(az), ephem.degrees(alt)
-            tl.date = str(ephem.Date(date_util.get_ephdate(datetime.fromtimestamp(sec), tzone=time_zone)))
-            ra_dec[ti] = tl.radec_of(az, alt) # in radians, a point in the sky above the observer
+            aa.set_jultime(rt['jul_date'].local_data[ti])
+            ra_dec[ti] = aa.radec_of(az, alt) # in radians, a point in the sky above the observer
 
         if rt.main_data_dist_axis == 0:
             az_alt = mpiarray.MPIArray.wrap(az_alt, axis=0)
