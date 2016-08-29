1581f0bc246652c07830b492120c647dbf0ca7ae
zuoshifan <zuoshifan@163.com>
2016-08-29 17:18:26 +0800

    Add RA as y_axis in plot_waterfall

--- tlpipe/plot/plot_waterfall.py
+++ tlpipe/plot/plot_waterfall.py
@@ -17,6 +17,7 @@ def plot(vis, li, gi, bl, obj, **kwargs):
     bl_incl = kwargs.get('bl_incl', 'all')
     bl_excl = kwargs.get('bl_excl', [])
     flag_ns = kwargs.get('flag_ns', False)
+    y_axis = kwargs.get('y_axis', 'jul_date')
     plot_abs = kwargs.get('plot_abs', False)
     fig_prefix = kwargs.get('fig_name', 'vis')
 
@@ -35,8 +36,13 @@ def plot(vis, li, gi, bl, obj, **kwargs):
         vis1 = vis
 
     freq = obj.freq[:]
-    jul_date = obj.time[:]
-    extent = [freq[0], freq[-1], jul_date[0], jul_date[-1]]
+    if y_axis == 'jul_date':
+        y_aixs = obj.time[:]
+        y_label = r'$t$ / Julian Date'
+    elif y_axis == 'ra':
+        y_aixs = obj['ra_dec'][:, 0]
+        y_label = r'RA / radian'
+    extent = [freq[0], freq[-1], y_aixs[0], y_aixs[-1]]
 
     plt.figure()
     if plot_abs:
@@ -45,7 +51,7 @@ def plot(vis, li, gi, bl, obj, **kwargs):
         fig, axarr = plt.subplots(1, 2, sharey=True)
     im = axarr[0].imshow(vis1.real, extent=extent, origin='lower', aspect='auto')
     axarr[0].set_xlabel(r'$\nu$ / MHz')
-    axarr[0].set_ylabel(r'$t$ / Julian Date')
+    axarr[0].set_ylabel(y_label)
     plt.colorbar(im, ax=axarr[0])
     im = axarr[1].imshow(vis1.imag, extent=extent, origin='lower', aspect='auto')
     axarr[1].set_xlabel(r'$\nu$ / MHz')
@@ -73,6 +79,7 @@ class PlotRawTimestream(tod_task.SingleRawTimestream):
                     'bl_incl': 'all', # or a list of include (bl1, bl2)
                     'bl_excl': [],
                     'flag_ns': False,
+                    'y_axis': 'jul_date', # or 'ra'
                     'plot_abs': False,
                     'fig_name': 'vis',
                   }
@@ -83,9 +90,11 @@ class PlotRawTimestream(tod_task.SingleRawTimestream):
         bl_incl = self.params['bl_incl']
         bl_excl = self.params['bl_excl']
         flag_ns = self.params['flag_ns']
+        y_axis = self.params['y_axis']
         plot_abs = self.params['plot_abs']
         fig_name = self.params['fig_name']
-        rt.bl_data_operate(plot, full_data=True, keep_dist_axis=False, bl_incl=bl_incl, bl_excl=bl_excl, fig_name=fig_name, flag_ns=flag_ns, plot_abs=plot_abs)
+
+        rt.bl_data_operate(plot, full_data=True, keep_dist_axis=False, bl_incl=bl_incl, bl_excl=bl_excl, fig_name=fig_name, flag_ns=flag_ns, y_axis=y_axis, plot_abs=plot_abs)
         rt.add_history(self.history)
 
         return rt
@@ -98,6 +107,7 @@ class PlotTimestream(tod_task.SingleTimestream):
                     'bl_incl': 'all', # or a list of include (bl1, bl2)
                     'bl_excl': [],
                     'flag_ns': False,
+                    'y_axis': 'jul_date', # or 'ra'
                     'plot_abs': False,
                     'fig_name': 'vis',
                   }
@@ -108,9 +118,11 @@ class PlotTimestream(tod_task.SingleTimestream):
         bl_incl = self.params['bl_incl']
         bl_excl = self.params['bl_excl']
         flag_ns = self.params['flag_ns']
+        y_axis = self.params['y_axis']
         plot_abs = self.params['plot_abs']
         fig_name = self.params['fig_name']
-        ts.pol_and_bl_data_operate(plot, full_data=True, keep_dist_axis=False, bl_incl=bl_incl, bl_excl=bl_excl, fig_name=fig_name, flag_ns=flag_ns, plot_abs=plot_abs)
+
+        ts.pol_and_bl_data_operate(plot, full_data=True, keep_dist_axis=False, bl_incl=bl_incl, bl_excl=bl_excl, fig_name=fig_name, flag_ns=flag_ns, y_axis=y_axis, plot_abs=plot_abs)
         ts.add_history(self.history)
 
         return ts
