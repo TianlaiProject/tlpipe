0d8bb3d5f10ac456b45edf5f6cea424f3c8ab05f
zuoshifan <zuoshifan@163.com>
2016-08-29 17:15:52 +0800

    Mask completed vis in re_order

--- tlpipe/timestream/re_order.py
+++ tlpipe/timestream/re_order.py
@@ -26,6 +26,7 @@ class ReOrder(tod_task.SingleTimestream):
             # first complete main data vis
             vis = np.zeros((num_int,)+ts['vis'].local_shape[1:], dtype=ts['vis'].dtype)
             vis[:num_phi] = ts['vis'].local_data[:]
+            vis[num_phi:] = complex(np.nan, np.nan) # mask the completed data
             vis = mpiarray.MPIArray.wrap(vis, axis=ts['vis'].distributed_axis)
             ts.create_main_data(vis, recreate=True, copy_attrs=True)
 
@@ -74,6 +75,6 @@ class ReOrder(tod_task.SingleTimestream):
 
         ts.add_history(self.history)
 
-        ts.info()
+        # ts.info()
 
         return ts
