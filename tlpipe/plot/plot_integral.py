af809fb38ef60ee08869e47af8155d0e76274dc1
zuoshifan <zuoshifan@163.com>
2016-11-14 21:41:42 +0800

    Make plot_integral works for Timestream too

--- tlpipe/plot/plot_integral.py
+++ tlpipe/plot/plot_integral.py
@@ -3,18 +3,20 @@
 import numpy as np
 from tlpipe.timestream import tod_task
 from tlpipe.timestream.raw_timestream import RawTimestream
+from tlpipe.timestream.timestream import Timestream
 from tlpipe.utils.path_util import output_path
 import matplotlib.pyplot as plt
 
 
-def plot(vis, vis_mask, li, gi, bl, obj, **kwargs):
+def plot(vis, vis_mask, li, gi, bl, ts, **kwargs):
 
-    if isinstance(bl, tuple): # for Timestream
-        pol = bl[0]
-        bl = tuple(bl[1])
-    else: # for RawTimestream
-        pol = None
-        bl = tuple(bl)
+    # if isinstance(bl, tuple): # for Timestream
+    #     pol = bl[0]
+    #     bl = tuple(bl[1])
+    # else: # for RawTimestream
+    #     pol = None
+    #     bl = tuple(bl)
+
     integral = kwargs.get('integral', 'time')
     bl_incl = kwargs.get('bl_incl', 'all')
     bl_excl = kwargs.get('bl_excl', [])
@@ -22,30 +24,39 @@ def plot(vis, vis_mask, li, gi, bl, obj, **kwargs):
     flag_ns = kwargs.get('flag_ns', True)
     fig_prefix = kwargs.get('fig_name', 'int')
     tag_output_iter= kwargs.get('tag_output_iter', True)
-    iteration= kwargs.get('iteration', 0)
+    iteration= kwargs.get('iteration', None)
+
+    if isinstance(ts, Timestream): # for Timestream
+        pol = bl[0]
+        bl = tuple(bl[1])
+    elif isinstance(ts, RawTimestream): # for RawTimestream
+        pol = None
+        bl = tuple(bl)
+    else:
+        raise ValueError('Need either a RawTimestream or Timestream')
 
     if bl_incl != 'all':
         bl1 = set(bl)
         bl_incl = [ {f1, f2} for (f1, f2) in bl_incl ]
         bl_excl = [ {f1, f2} for (f1, f2) in bl_excl ]
         if (not bl1 in bl_incl) or (bl1 in bl_excl):
-            return vis
+            return vis, vis_mask
 
     if flag_mask:
         vis1 = np.ma.array(vis, mask=vis_mask)
     elif flag_ns:
         vis1 = vis.copy()
-        on = np.where(obj['ns_on'][:])[0]
+        on = np.where(ts['ns_on'][:])[0]
         vis1[on] = complex(np.nan, np.nan)
     else:
         vis1 = vis
 
     if integral == 'time':
-        ax_val = obj.freq[:]
+        ax_val = ts.freq[:]
         vis1 = np.ma.mean(np.ma.masked_invalid(vis1), axis=0)
         xlabel = r'$\nu$ / MHz'
     elif integral == 'freq':
-        ax_val = obj.time[:]
+        ax_val = ts.time[:]
         vis1 = np.ma.mean(np.ma.masked_invalid(vis1), axis=1)
         xlabel = r'$t$ / Julian Date'
     else:
@@ -61,7 +72,10 @@ def plot(vis, vis_mask, li, gi, bl, obj, **kwargs):
     axarr[2].legend()
     axarr[2].set_xlabel(xlabel)
 
-    fig_name = '%s_%s_%d_%d.png' % (fig_prefix, integral, bl[0], bl[1])
+    if pol is None:
+        fig_name = '%s_%s_%d_%d.png' % (fig_prefix, integral, bl[0], bl[1])
+    else:
+        fig_name = '%s_%s_%d_%d_%s.png' % (fig_prefix, integral, bl[0], bl[1], pol)
     if tag_output_iter:
         fig_name = output_path(fig_name, iteration=iteration)
     else:
@@ -86,9 +100,7 @@ class Plot(tod_task.TaskTimestream):
 
     prefix = 'pit_'
 
-    def process(self, rt):
-
-        assert isinstance(rt, RawTimestream), '%s only works for RawTimestream object currently' % self.__class__.__name__
+    def process(self, ts):
 
         integral = self.params['integral']
         bl_incl = self.params['bl_incl']
@@ -98,8 +110,15 @@ class Plot(tod_task.TaskTimestream):
         fig_name = self.params['fig_name']
         tag_output_iter = self.params['tag_output_iter']
 
-        rt.bl_data_operate(plot, full_data=True, keep_dist_axis=False, integral=integral, bl_incl=bl_incl, bl_excl=bl_excl, fig_name=fig_name, iteration=self.iteration, tag_output_iter=tag_output_iter, flag_mask=flag_mask, flag_ns=flag_ns)
+        ts.redistribute('baseline')
 
-        rt.add_history(self.history)
+        if isinstance(ts, RawTimestream):
+            func = ts.bl_data_operate
+        elif isinstance(ts, Timestream):
+            func = ts.pol_and_bl_data_operate
 
-        return rt
+        func(plot, full_data=True, keep_dist_axis=False, integral=integral, bl_incl=bl_incl, bl_excl=bl_excl, fig_name=fig_name, iteration=self.iteration, tag_output_iter=tag_output_iter, flag_mask=flag_mask, flag_ns=flag_ns)
+
+        ts.add_history(self.history)
+
+        return ts
