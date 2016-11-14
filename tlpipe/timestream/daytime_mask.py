f1f1c30d51e5113472fc35d4cf843b10e0df0dbd
zuoshifan <zuoshifan@163.com>
2016-11-14 16:24:09 +0800

    New daytime_mask

---
+++ tlpipe/timestream/daytime_mask.py
@@ -0,0 +1,28 @@
+"""Daytime data mask."""
+
+import numpy as np
+import tod_task
+
+
+class Mask(tod_task.TaskTimestream):
+    """Daytime data mask."""
+
+    params_init = {
+                    'mask_time_range': [8.0, 19.5], # hour
+                  }
+
+    prefix = 'dm_'
+
+    def process(self, ts):
+
+        mask_time_range = self.params['mask_time_range']
+
+        local_hour = ts['local_hour'].local_data
+        day_inds = np.where(np.logical_and(local_hour>=mask_time_range[0], local_hour<=mask_time_range[1]))[0]
+        ts.local_vis_mask[day_inds] = True # do not change vis directly
+
+        ts.add_history(self.history)
+
+        # ts.info()
+
+        return ts
