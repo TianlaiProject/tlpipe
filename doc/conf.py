f6794f009523e1ddf29dafbe986a8dff4766c463
zuoshifan <zuoshifan@163.com>
2016-12-06 15:24:38 +0800

    RTD can not install cora, use mock instead

--- doc/conf.py
+++ doc/conf.py
@@ -15,11 +15,28 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
 # sys.path.append(os.path.abspath('.'))
+
+# Check if we are on readthedocs
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+# Mock up modules missing on readthedocs.
+from mock import Mock as MagicMock
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+# Do not mock up mpi4py. This is an "extra", and docs build without it.
+# MOCK_MODULES = ['cora', 'mpi4py']
+MOCK_MODULES = ['cora',]
+if on_rtd:
+    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # -- General configuration ------------------------------------------------
 
