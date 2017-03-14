3e48f73fe4d2c39ad1c2c0e71fedac1eea88a087
zuoshifan <zuoshifan@163.com>
2017-03-14 21:06:36 +0800

    Some work in rpca_decomp

--- tlpipe/utils/rpca_decomp.py
+++ tlpipe/utils/rpca_decomp.py
@@ -6,13 +6,27 @@ def MAD(a):
     return np.median(np.abs(a - np.median(a))) / 0.6745
 
 
+def l0_norm(a):
+    return len(np.where(a.flatten() != 0.0)[0])
+
+
+def l1_norm(a):
+    return np.sum(np.abs(a))
+
+
 def truncate(a, lmbda):
     return a * (np.abs(a) > lmbda)
 
 
+def sign(a):
+    if np.isrealobj(a):
+        return np.sign(a)
+    else:
+        return np.exp(1.0J * np.angle(a))
+
+
 def shrink(a, lmbda):
-    # return np.sign(a) * np.maximum(np.abs(a) - lmbda, 0.0) # works only for real a
-    return a * np.maximum(1.0 - lmbda / np.abs(a), 0.0)
+    return sign(a) * np.maximum(np.abs(a) - lmbda, 0.0) # work for both real and complex
 
 
 def decompose(V, rank=1, lmbda=None, threshold='hard', max_iter=100, tol=1.0e-8, check_hermitian=False, debug=False):
@@ -31,13 +45,14 @@ def decompose(V, rank=1, lmbda=None, threshold='hard', max_iter=100, tol=1.0e-8,
         fixed_lmbda = True
 
     S = np.zeros_like(V)
-    NF_old = np.Inf
+    opt_old = np.Inf
 
+    min_opt = np.Inf
     min_tol = np.Inf
     if not fixed_lmbda:
         min_lmbda = lmbda
         lmbdas = [ lmbda ]
-        tols = [ np.Inf ]
+        opts= [ np.Inf ]
 
     for it in xrange(max_iter):
         # compute only the largest rank eigen values and vectors, which is faster
@@ -47,29 +62,35 @@ def decompose(V, rank=1, lmbda=None, threshold='hard', max_iter=100, tol=1.0e-8,
         V0 = np.dot(U*np.maximum(s, 0), U.T.conj())
         res = V - V0
         N = res - S
-        NF = la.norm(N, ord='fro')
-        tol1 = np.abs(NF_old - NF) / NF
-        if tol1 < tol:
+        if threshold == 'hard':
+            opt = 0.5 * la.norm(N, ord='fro')**2 + lmbda * l0_norm(S)
+        elif threshold == 'soft':
+            opt = 0.5 * la.norm(N, ord='fro')**2 + lmbda * l1_norm(S)
+        else:
+            raise ValueError('Unknown thresholding method: %s' % threshold)
+        tol1 = opt_old - opt
+        if tol1 >= 0.0 and tol1 < tol:
             if debug:
-                print 'Converge when iteration: %d with tol: %g < %g' % (it, tol1, tol)
+                print 'Converge when iteration: %d, opt: %g, with tol: %g < %g' % (it, opt, tol1, tol)
             break
 
-        # record the minimum tol and its corresponding V0, S and lmbda
-        if tol1 < min_tol:
-            min_tol = tol1
+        # record the minimum opt and its corresponding tol, V0, S and lmbda
+        if opt < min_opt:
+            min_opt = opt
+            min_tol = tol1 # tol corresponding to this opt
             min_V0 = V0
             min_S = S
             if not fixed_lmbda:
                 min_lmbda = lmbda
 
-        NF_old = NF
+        opt_old = opt
         if not fixed_lmbda and it >= 1:
             # lmbda = 5.0 * np.std(N)
             # use the universal threshold: sigma * (2 * log(d*d))**0.5
             lmbda = max(1.0, 2 * np.log(d)**0.5) * np.std(N)
 
             # avoid cycling around
-            while (lmbda in lmbdas) and (tol1 in tols):
+            while (lmbda in lmbdas) and (opt in opts):
                 while True:
                     factor = np.random.gamma(1.0)
                     if factor != 1.0 and (factor >=0.9 or factor <= 10.0):
@@ -79,16 +100,15 @@ def decompose(V, rank=1, lmbda=None, threshold='hard', max_iter=100, tol=1.0e-8,
             if debug:
                 print 'lmbda:', lmbda
             lmbdas.append(lmbda)
-            tols.append(tol1)
+            opts.append(opt)
 
         if threshold == 'hard':
             S = truncate(res, lmbda)
         elif threshold == 'soft':
             S = shrink(res, lmbda)
-        else:
-            raise ValueError('Unknown thresholding method: %s' % threshold)
+
     else:
-        print 'Exit with max_iter: %d, tol: %g >= %g' % (it, min_tol, tol)
+        print 'Exit with max_iter: %d, opt: %g, tol: %g >= %g' % (it, min_opt, min_tol, tol)
         return min_V0, min_S
 
     return V0, S
