b82a843b4a39cd0bd25e775025da60ed846cebf6
zuoshifan <zuoshifan@163.com>
2017-03-15 15:07:53 +0800

    New criteria in rpca_decomp.py

--- tlpipe/utils/rpca_decomp.py
+++ tlpipe/utils/rpca_decomp.py
@@ -35,24 +35,17 @@ def decompose(V, rank=1, lmbda=None, threshold='hard', max_iter=100, tol=1.0e-8,
         if not np.allclose(V, V.T.conj()):
             raise ValueError('V must be a Hermitian matrix')
 
-    d = V.shape[0]
     if lmbda is None:
-        lmbda = max(1.0, 2 * np.log(d)**0.5) * MAD(V)
         fixed_lmbda = False
-        if debug:
-            print 'lmbda:', lmbda
     else:
         fixed_lmbda = True
 
+    d = V.shape[0]
     S = np.zeros_like(V)
     opt_old = np.Inf
 
     min_opt = np.Inf
     min_tol = np.Inf
-    if not fixed_lmbda:
-        min_lmbda = lmbda
-        lmbdas = [ lmbda ]
-        opts= [ np.Inf ]
 
     for it in xrange(max_iter):
         # compute only the largest rank eigen values and vectors, which is faster
@@ -62,14 +55,23 @@ def decompose(V, rank=1, lmbda=None, threshold='hard', max_iter=100, tol=1.0e-8,
         V0 = np.dot(U*np.maximum(s, 0), U.T.conj())
         res = V - V0
         N = res - S
+
+        if not fixed_lmbda:
+            # use the universal threshold: sigma * (2 * log(d*d))**0.5
+            lmbda = max(1.0, 2 * np.log(d)**0.5) * min(MAD(N), np.std(N))
+            if debug:
+                print 'lmbda:', lmbda
+
         if threshold == 'hard':
             opt = 0.5 * la.norm(N, ord='fro')**2 + lmbda * l0_norm(S)
         elif threshold == 'soft':
             opt = 0.5 * la.norm(N, ord='fro')**2 + lmbda * l1_norm(S)
         else:
             raise ValueError('Unknown thresholding method: %s' % threshold)
+
         tol1 = opt_old - opt
-        if tol1 >= 0.0 and tol1 < tol:
+        # if tol1 >= 0.0 and tol1 < tol:
+        if abs(tol1) < tol:
             if debug:
                 print 'Converge when iteration: %d, opt: %g, with tol: %g < %g' % (it, opt, tol1, tol)
             break
@@ -80,27 +82,8 @@ def decompose(V, rank=1, lmbda=None, threshold='hard', max_iter=100, tol=1.0e-8,
             min_tol = tol1 # tol corresponding to this opt
             min_V0 = V0
             min_S = S
-            if not fixed_lmbda:
-                min_lmbda = lmbda
 
         opt_old = opt
-        if not fixed_lmbda and it >= 1:
-            # lmbda = 5.0 * np.std(N)
-            # use the universal threshold: sigma * (2 * log(d*d))**0.5
-            lmbda = max(1.0, 2 * np.log(d)**0.5) * np.std(N)
-
-            # avoid cycling around
-            while (lmbda in lmbdas) and (opt in opts):
-                while True:
-                    factor = np.random.gamma(1.0)
-                    if factor != 1.0 and (factor >=0.9 or factor <= 10.0):
-                        break
-                lmbda = factor * min_lmbda
-
-            if debug:
-                print 'lmbda:', lmbda
-            lmbdas.append(lmbda)
-            opts.append(opt)
 
         if threshold == 'hard':
             S = truncate(res, lmbda)
