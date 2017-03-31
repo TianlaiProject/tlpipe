59a9953bc7f16a66ab59eafd085f267d1aa21bce
zuoshifan <zuoshifan@163.com>
2017-03-31 15:43:17 +0800

    Changes in bad_detect

--- tlpipe/timestream/bad_detect.py
+++ tlpipe/timestream/bad_detect.py
@@ -12,6 +12,7 @@ import itertools
 import numpy as np
 import tod_task
 from raw_timestream import RawTimestream
+from timestream import Timestream
 from caput import mpiutil
 
 
@@ -24,72 +25,63 @@ class Detect(tod_task.TaskTimestream):
 
     """
 
-    params_init = {
-                    'num_auto': 6, # least number of feeds to check auto-correlation
-                    'threshold': 2.24, # threshold of the MAD-median rule
-                  }
+    params_init = {}
 
     prefix = 'bd_'
 
-    def process(self, rt):
+    def process(self, ts):
 
-        assert isinstance(rt, RawTimestream), '%s only works for RawTimestream object currently' % self.__class__.__name__
+        ts.redistribute('baseline')
 
-        num_auto = self.params['num_auto']
-        threshold = self.params['threshold']
-
-        rt.redistribute('time')
-
-        vis = rt.local_vis
-        vis_mask = rt.local_vis_mask
+        vis = ts.local_vis
+        vis_mask = ts.local_vis_mask
 
         # mask non-finite vis values
         vis_mask[:] = np.where(np.isfinite(vis), vis_mask, True)
 
-        xx_auto = []
-        yy_auto = []
         # mask where the imaginary part of an auto-correlation is non-zero
-        for bi, (fi, fj) in enumerate(rt.local_bl):
-            if fi == fj:
-                vis_mask[..., bi] = np.where(vis[..., bi].imag == 0.0, vis_mask[..., bi], True)
-                if fi % 2 == 1:
-                    xx_auto.append(bi)
-                else:
-                    yy_auto.append(bi)
-
-        # mask values exceed given threshold by using the MAD-median rule
-        # see Wilcox, 2014, Modern robust statistical methods can provide substantially higher power and a deeper understanding of data
-        for auto in [xx_auto, yy_auto]:
-            if len(auto) >= num_auto:
-                vis1 = np.ma.array(vis[..., auto].real, mask=vis_mask[..., auto])
-                median = np.ma.median(vis1, axis=2)
-                abs_diff = np.abs(vis1-median[:, :, np.newaxis])
-                mad = np.ma.median(abs_diff, axis=2) / 0.6745
-                cnt = vis1.count(axis=2)
-                mask1 = vis_mask[..., auto].copy()
-                # avoid statistical error for small cnt
-                cond = np.logical_and(abs_diff>threshold*mad[:, :, np.newaxis], np.tile(cnt[:, :, np.newaxis], (1, 1, len(auto)))>=num_auto)
-                mask1 = np.where(cond, True, mask1)
-
-                vis_mask[..., auto] = mask1 # replace with the new mask
-
-        rt.redistribute('baseline')
+        if isinstance(ts, RawTimestream):
+            for bi, (fi, fj) in enumerate(ts.local_bl):
+                if fi == fj:
+                    vis_mask[..., bi] = np.where(vis[..., bi].imag == 0.0, vis_mask[..., bi], True)
+        elif isinstance(ts, Timestream):
+            for bi, (fi, fj) in enumerate(ts.local_bl):
+                for pi, pol in enumerate(ts.local_pol):
+                    pol = ts.pol_dict[pol]
+                    if fi == fj and pol in ['xx', 'yy']:
+                        vis_mask[..., pi, bi] = np.where(vis[..., pi, bi].imag == 0.0, vis_mask[..., pi, bi], True)
 
         # mask bl that have no signal
         problematic_bls = []
         bad_bls = []
-        for bi in xrange(len(rt.local_bl)):
-            # create a copy of vis for this bi, and fill 0 in masked positions
-            vis1 = np.where(rt.local_vis_mask[..., bi], 0, rt.local_vis[..., bi])
-            if np.sum(rt.local_vis_mask) >= 0.5 * np.prod(rt.local_vis.shape):
-                bl = tuple(rt.local_bl[bi])
-                problematic_bls.append(bl)
-                # print 'Problematic baseline: (%d, %d)' % bl
-            if np.allclose(vis1, 0): # all zeros
-                rt.local_vis_mask[..., bi] = True
-                bl = tuple(rt.local_bl[bi])
-                bad_bls.append(bl)
-                # print 'Bad baseline: (%d, %d)' % bl
+        for bi in xrange(len(ts.local_bl)):
+            if isinstance(ts, RawTimestream):
+                # create a copy of vis for this bi, and fill 0 in masked positions
+                vis1 = np.where(ts.local_vis_mask[..., bi], 0, ts.local_vis[..., bi])
+                if np.sum(ts.local_vis_mask[..., bi]) >= 0.5 * np.prod(ts.local_vis[..., bi].shape):
+                    bl = tuple(ts.local_bl[bi])
+                    problematic_bls.append(bl)
+                    # print 'Problematic baseline: (%d, %d)' % bl
+                if np.allclose(vis1, 0): # all zeros
+                    ts.local_vis_mask[..., bi] = True # mask all
+                    bl = tuple(ts.local_bl[bi])
+                    bad_bls.append(bl)
+                    # print 'Bad baseline: (%d, %d)' % bl
+            elif isinstance(ts, Timestream):
+                for pi, pol in enumerate(ts.local_pol):
+                    pol = ts.pol_dict[pol]
+                    # create a copy of vis for this bi, and fill 0 in masked positions
+                    vis1 = np.where(ts.local_vis_mask[..., pi, bi], 0, ts.local_vis[..., pi, bi])
+                    if np.sum(ts.local_vis_mask[..., pi, bi]) >= 0.5 * np.prod(ts.local_vis[..., pi, bi].shape):
+                        bl = tuple(ts.local_bl[bi])
+                        problematic_bls.append((bl, pol))
+                        # print 'Problematic baseline: (%d, %d)' % bl
+                    if np.allclose(vis1, 0): # all zeros
+                        ts.local_vis_mask[..., pi, bi] = True # mask all
+                        bl = tuple(ts.local_bl[bi])
+                        bad_bls.append((bl, pol))
+                        # print 'Bad baseline: (%d, %d)' % bl
+
 
         # gather list
         comm = mpiutil.world
@@ -101,4 +93,4 @@ class Detect(tod_task.TaskTimestream):
             print 'Problematic baseline: ', problematic_bls
             print 'Bad baseline: ', bad_bls
 
-        return super(Detect, self).process(rt)
+        return super(Detect, self).process(ts)
