1c2d54bb22fa31abb58773a1f538832c1fee351d
zuoshifan <zuoshifan@163.com>
2017-04-01 09:46:22 +0800

    Find bad channels or feeds in bad_detect

--- tlpipe/timestream/bad_detect.py
+++ tlpipe/timestream/bad_detect.py
@@ -55,32 +55,25 @@ class Detect(tod_task.TaskTimestream):
         problematic_bls = []
         bad_bls = []
         for bi in xrange(len(ts.local_bl)):
+            bl = tuple(ts.local_bl[bi])
             if isinstance(ts, RawTimestream):
                 # create a copy of vis for this bi, and fill 0 in masked positions
                 vis1 = np.where(ts.local_vis_mask[..., bi], 0, ts.local_vis[..., bi])
                 if np.sum(ts.local_vis_mask[..., bi]) >= 0.5 * np.prod(ts.local_vis[..., bi].shape):
-                    bl = tuple(ts.local_bl[bi])
                     problematic_bls.append(bl)
-                    # print 'Problematic baseline: (%d, %d)' % bl
                 if np.allclose(vis1, 0): # all zeros
                     ts.local_vis_mask[..., bi] = True # mask all
-                    bl = tuple(ts.local_bl[bi])
                     bad_bls.append(bl)
-                    # print 'Bad baseline: (%d, %d)' % bl
             elif isinstance(ts, Timestream):
                 for pi, pol in enumerate(ts.local_pol):
                     pol = ts.pol_dict[pol]
                     # create a copy of vis for this bi, and fill 0 in masked positions
                     vis1 = np.where(ts.local_vis_mask[..., pi, bi], 0, ts.local_vis[..., pi, bi])
                     if np.sum(ts.local_vis_mask[..., pi, bi]) >= 0.5 * np.prod(ts.local_vis[..., pi, bi].shape):
-                        bl = tuple(ts.local_bl[bi])
                         problematic_bls.append((bl, pol))
-                        # print 'Problematic baseline: (%d, %d)' % bl
                     if np.allclose(vis1, 0): # all zeros
                         ts.local_vis_mask[..., pi, bi] = True # mask all
-                        bl = tuple(ts.local_bl[bi])
                         bad_bls.append((bl, pol))
-                        # print 'Bad baseline: (%d, %d)' % bl
 
 
         # gather list
@@ -89,8 +82,80 @@ class Detect(tod_task.TaskTimestream):
             problematic_bls = list(itertools.chain(*comm.allgather(problematic_bls)))
             bad_bls = list(itertools.chain(*comm.allgather(bad_bls)))
 
+        # try to find bad channels or feeds
+        if isinstance(ts, RawTimestream):
+            problematic_chs = {}
+            bad_chs = {}
+            for ch1, ch2 in problematic_bls:
+                problematic_chs.setdefault(ch1, 0)
+                problematic_chs.setdefault(ch2, 0)
+                problematic_chs[ch1] += 1
+                problematic_chs[ch2] += 1
+            for ch1, ch2 in bad_bls:
+                bad_chs.setdefault(ch1, 0)
+                bad_chs.setdefault(ch2, 0)
+                bad_chs[ch1] += 1
+                bad_chs[ch2] += 1
+
+            num_chs = len(ts['channo'].flatten())
+            pchs = []
+            bchs = []
+            for ch, val in problematic_chs.iteritems():
+                if val > 0.5 * num_chs:
+                    pchs.append(ch)
+            for ch, val in bad_chs.iteritems():
+                if val > 0.5 * num_chs:
+                    bchs.append(ch)
+
+            # set mask for bad channels
+            for bi, (ch1, ch2) in enumerate(ts.local_bl):
+                if ch1 in bchs or ch2 in bchs:
+                    ts.local_vis_mask[..., bi] = True
+
+        elif isinstance(ts, Timestream):
+            problematic_feeds = {}
+            bad_feeds = {}
+            for (fd1, fd2), pol in problematic_bls:
+                problematic_feeds.setdefault(pol, {})
+                problematic_feeds[pol].setdefault(fd1, 0)
+                problematic_feeds[pol].setdefault(fd2, 0)
+                problematic_feeds[pol][fd1] += 1
+                problematic_feeds[pol][fd2] += 1
+            for (fd1, fd2), pol in bad_bls:
+                bad_feeds.setdefault(pol, {})
+                bad_feeds[pol].setdefault(fd1, 0)
+                bad_feeds[pol].setdefault(fd2, 0)
+                bad_feeds[pol][fd1] += 1
+                bad_feeds[pol][fd2] += 1
+
+            num_feeds = len(ts['feedno'])
+            pfeeds = []
+            bfeeds = []
+            for pol, d in problematic_feeds.iteritems():
+                for fd, val in d.iteritems():
+                    if val > 0.5 * num_feeds:
+                        pfeeds.append((fd, pol))
+            for pol, d in bad_feeds.iteritems():
+                for fd, val in d.iteritems():
+                    if val > 0.5 * num_feeds:
+                        bfeeds.append((fd, pol))
+
+            # set mask for bad feeds and pol
+            for bi, (fd1, fd2) in enumerate(ts.local_bl):
+                for pi, pol in enumerate(ts.local_pol):
+                    pol = ts.pol_dict[pol]
+                    if (fd1, pol) in bfeeds or (fd2, pol) in bfeeds:
+                        ts.local_vis_mask[..., pi, bi] = True
+
+
         if mpiutil.rank0:
             print 'Problematic baseline: ', problematic_bls
             print 'Bad baseline: ', bad_bls
+            if isinstance(ts, RawTimestream):
+                print 'Problematic channels: ', pchs
+                print 'Bad channels: ', bchs
+            elif isinstance(ts, Timestream):
+                print 'Problematic feeds: ', pfeeds
+                print 'Bad feeds: ', bfeeds
 
         return super(Detect, self).process(ts)
