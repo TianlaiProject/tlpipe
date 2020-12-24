9e3b2c64abaa96feefad6afa282dfc60388099fb
zuoshifan <zuoshifan@163.com>
2020-12-24 21:28:44 +0800

    Add new channel_mask and feed_mask

---
+++ tlpipe/timestream/channel_mask.py
@@ -0,0 +1,40 @@
+"""Mask the data involving the specified channels.
+
+Inheritance diagram
+-------------------
+
+.. inheritance-diagram:: Mask
+   :parts: 2
+
+"""
+
+import numpy as np
+import ephem
+import aipy as a
+import timestream_task
+from tlpipe.container.raw_timestream import RawTimestream
+
+
+class Mask(timestream_task.TimestreamTask):
+    """Mask the data involving the specified channels."""
+
+    params_init = {
+                    'channels': [] # in format [ channo1, channo2, ... ]
+                  }
+
+    prefix = 'cm_'
+
+    def process(self, ts):
+
+        assert isinstance(ts, RawTimestream), '%s only works for RawTimestream object, use %s instead for Timestream object' % (self.__class__.__name__, 'feed_mask.py')
+
+        channels = self.params['channels']
+
+        for channo in channels:
+            for bi, (ch1, ch2) in enumerate(ts.local_bl):
+                if channo == ch1 or channo == ch2:
+                    # print 'mask for bi = %d, (%d, %d)' % (bi, ch1, ch2)
+                    ts.local_vis_mask[:, :, bi] = True
+
+
+        return super(Mask, self).process(ts)
---
+++ tlpipe/timestream/feed_mask.py
@@ -0,0 +1,48 @@
+"""Mask the data involving the specified feeds.
+
+Inheritance diagram
+-------------------
+
+.. inheritance-diagram:: Mask
+   :parts: 2
+
+"""
+
+import numpy as np
+import ephem
+import aipy as a
+import timestream_task
+from tlpipe.container.timestream import Timestream
+
+
+class Mask(timestream_task.TimestreamTask):
+    """Mask the data involving the specified feeds."""
+
+    params_init = {
+                    'feeds': [] # in format [ (feedno, pol), (3, 'x'), (5, 'y), ... ]
+                  }
+
+    prefix = 'fm_'
+
+    def process(self, ts):
+
+        assert isinstance(ts, Timestream), '%s only works for Timestream object, use %s instead for RawTimestream object' % (self.__class__.__name__, 'channel_mask.py')
+
+        feeds = self.params['feeds']
+
+        for feedno, pol in feeds:
+            for i in [0, 1]:
+                bis = np.where(ts.local_bl[:, i] == feedno)[0].tolist()
+                # bis = list(set(bis)) # make unique
+                pis = []
+                for pi, lp in enumerate(ts.local_pol):
+                    if pol == ts.pol_dict[lp][i]:
+                        pis.append(pi)
+
+                for bi in bis:
+                    for pi in pis:
+                        # print 'mask for bl = (%d, %d), pol = %s' % (ts.local_bl[bi, 0], ts.local_bl[bi, 1], ts.pol_dict[ts.local_pol[pi]])
+                        ts.local_vis_mask[:, :, pi, bi] = True
+
+
+        return super(Mask, self).process(ts)
