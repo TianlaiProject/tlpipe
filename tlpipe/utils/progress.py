8d62d9801613968fb4fec4fed647d5fbc571f5f6
zuoshifan <zuoshifan@163.com>
2021-03-06 09:56:53 +0800

    Output rank and hostname info in progress

--- tlpipe/utils/progress.py
+++ tlpipe/utils/progress.py
@@ -1,10 +1,12 @@
+import socket
+
 class Progress(object):
 
-    def __init__(self, length, step=None, prog_msg = None):
+    def __init__(self, length, step=None, prog_msg=None, comm=None):
         if not prog_msg is None:
             self.msg = prog_msg
         else:
-            self.msg = 'Progress: %d of ' +  ('%d...' % length)
+            self.msg = 'Progress: %d of ' +  ('%d%s in %s...' % (length, ' for process %s' % comm.rank if comm else '', socket.gethostname()))
 
         if step is None:
             if length < 10:
@@ -26,26 +28,37 @@ class Progress(object):
 
 
 if __name__ == '__main__':
+    from caput import mpiutil
+    comm = mpiutil.world
+    # comm = None
+
     lg = 7
-    pg = Progress(lg, step=2)
+    if mpiutil.rank0:
+        pg = Progress(lg, step=2, comm=comm)
+    for i in range(lg):
+        if mpiutil.rank0:
+            pg.show(i)
+
+    lg = 7
+    pg = Progress(lg, step=2, comm=comm)
     for i in range(lg):
         pg.show(i)
 
-    pg1 = Progress(lg, step=None)
+    pg1 = Progress(lg, step=None, comm=comm)
     for i in range(lg):
         pg1.show(i)
 
     lg = 20
-    pg2 = Progress(lg, step=None)
+    pg2 = Progress(lg, step=None, comm=comm)
     for i in range(lg):
         pg2.show(i)
 
     lg = 23
-    pg3 = Progress(lg, step=None)
+    pg3 = Progress(lg, step=None, comm=comm)
     for i in range(lg):
         pg3.show(i)
 
     lg = 10106
-    pg4 = Progress(lg, step=None)
+    pg4 = Progress(lg, step=None, comm=comm)
     for i in range(lg):
         pg4.show(i)
