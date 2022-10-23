f46561137db2d4e43864bddfb3422672051ce091
zuoshifan <zuoshifan@163.com>
2022-10-23 22:51:32 +0800

    Correct bool conversion in scripts

--- tlpipe/utils/argparse_action.py
+++ tlpipe/utils/argparse_action.py
@@ -8,3 +8,12 @@ class EvalAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         values = eval(values)
         setattr(namespace, self.dest, values)
+
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Unsupported value encountered.')
