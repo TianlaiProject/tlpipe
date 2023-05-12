62e1a2f2ed3f7ba05a934403fb27fdbc109e5866
zuoshifan <zuoshifan@163.com>
2023-05-12 16:37:55 +0800

    Solve coupling in mean_subtract_time_ordered

--- tlpipe/timestream/mean_subtract_time_ordered.py
+++ tlpipe/timestream/mean_subtract_time_ordered.py
@@ -9,6 +9,7 @@ Inheritance diagram
 """
 
 import numpy as np
+from scipy import linalg as la
 import h5py
 from . import timestream_task
 from tlpipe.container.raw_timestream import RawTimestream
@@ -25,7 +26,8 @@ class Subtract(timestream_task.TimestreamTask):
     params_init = {
                     'time_range': [21.5, 5.5], # [t1, t2], local hour, use the mean of t1 < t < t2 if t1 < t2 or {t1 < t < 24.0 and 0.0 < t < t2} if t1 > t2
                     'save_night_mean': False,
-                    'night_mean_file': 'night_mean/mean.hdf5'
+                    'night_mean_file': 'night_mean/mean.hdf5',
+                    'solve_coupling': False
     }
 
     prefix = 'su_'
@@ -35,6 +37,7 @@ class Subtract(timestream_task.TimestreamTask):
         via_memmap = self.params['via_memmap']
         save_night_mean = self.params['save_night_mean']
         night_mean_file = self.params['night_mean_file']
+        solve_coupling = self.params['solve_coupling']
         tag_output_iter = self.params['tag_output_iter']
 
         ts.redistribute('time', via_memmap=via_memmap)
@@ -95,6 +98,83 @@ class Subtract(timestream_task.TimestreamTask):
 
         night_mean = mpiutil.bcast(night_mean, comm=ts.comm)
 
-        ts.local_vis[:] -= night_mean[np.newaxis, ...]
+        if not solve_coupling:
+            ts.local_vis[:] -= night_mean[np.newaxis, ...]
+        else:
+            nf = len(ts.freq)
+            pol = [ ts.pol_dict[p] for p in ts['pol'][:] ] # as string
+            # gain_pd = {'xx': 0, 'yy': 1,    0: 'xx', 1: 'yy'} # for gain related op
+            bls = ts.bl[:]
+            feedno = ts['feedno'][:].tolist()
+            nfeed = len(feedno)
+
+            # find indices mapping between Vmat and vis
+            # bis = range(nbl)
+            bis_conj = [] # indices that shold be conj
+            mis = [] # indices in the nfeed x nfeed matrix by flatten it to a vector
+            mis_conj = [] # indices (of conj vis) in the nfeed x nfeed matrix by flatten it to a vector
+            for bi, (fdi, fdj) in enumerate(bls):
+                ai, aj = feedno.index(fdi), feedno.index(fdj)
+                mis.append(ai * nfeed + aj)
+                if ai != aj:
+                    bis_conj.append(bi)
+                    mis_conj.append(aj * nfeed + ai)
+
+            V1 = np.zeros((nfeed, nfeed), dtype=ts.vis.dtype) # Vmat before night-time mean subtract
+            Vc = np.zeros((nfeed, nfeed), dtype=ts.vis.dtype) # Vmat of the subtracted night-time mean
+
+            for ti in range(ts.local_vis.shape[0]):
+
+                # when noise on, just pass
+                if 'ns_on' in ts.keys() and ts['ns_on'].local_data[ti]:
+                    continue
+
+                for fi in range(nf):
+                    for pi, pol_str in enumerate(['xx', 'yy']):
+                        V1.flat[mis] = np.ma.array(ts.local_vis[ti, fi, pol.index(pol_str)], mask=ts.local_vis_mask[ti, fi, pol.index(pol_str)]).filled(0)
+                        V1.flat[mis_conj] = np.ma.array(ts.local_vis[ti, fi, pol.index(pol_str), bis_conj], mask=ts.local_vis_mask[ti, fi, pol.index(pol_str), bis_conj]).conj().filled(0)
+                        V1[~np.isfinite(V1)] = 0
+
+                        Vc.flat[mis] = night_mean[fi, pol.index(pol_str)]
+                        Vc.flat[mis_conj] = night_mean[fi, pol.index(pol_str), bis_conj].conj()
+                        Vc[~np.isfinite(Vc)] = 0
+
+                        V2 = V1 - Vc # Vmat after night-time mean subtract
+
+                        e1, U1 = la.eigh(V1)
+                        e1[e1<1.0e-12] = 0.0
+                        e2, U2 = la.eigh(V2)
+                        e2[e2<1.0e-12] = 0.0
+
+                        G = np.dot(U1*e1**0.5, la.pinv(U2*e2**0.5))
+
+                        V = np.dot(la.pinv(G), np.dot(V1, la.pinv(G.T.conj())))
+
+                        # one round diag correction to make diag elements close to each other
+                        # -------------------------------------------------------------------
+                        diags = V[np.diag_indices(V.shape[0])].real
+                        # print(np.mean(diags[diags>1.0e-6]), np.std(diags[diags>1.0e-6]))
+                        g = np.where(diags>1.0e-6, diags**0.5, 0.0)
+                        m = np.mean(diags[diags>1.0e-6])
+                        gg = np.outer(g, g)
+                        V = np.where(gg>1.0e-8, m*V/gg, 0.0)
+
+                        V2 = V
+
+                        e1, U1 = la.eigh(V1)
+                        e1[e1<1.0e-12] = 0.0
+                        e2, U2 = la.eigh(V2)
+                        e2[e2<1.0e-12] = 0.0
+
+                        G = np.dot(U1*e1**0.5, la.pinv(U2*e2**0.5))
+
+                        V = np.dot(la.pinv(G), np.dot(V1, la.pinv(G.T.conj())))
+                        # -------------------------------------------------------------------
+
+                        # make imag part of auto-correlation to be 0
+                        V[np.diag_indices(V.shape[0])] = V[np.diag_indices(V.shape[0])].real + 0j
+
+                        ts.local_vis[ti, fi, pol.index(pol_str)] = V.flat[mis]
+
 
         return super(Subtract, self).process(ts)
